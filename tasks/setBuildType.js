c418789f0d75976fb0d843eb657556c994c68087
Travis McHattie <travis.mchattie@thinksolid.com>
2012-10-19 12:06:30 -0400

    Removed outdated files

--- tasks/setBuildType.js
+++
@@ -1,9 +0,0 @@
-module.exports = function( grunt ) {
-
-    grunt.registerTask('setBuildType', function() {
-        var directory = this.args[0];
-        grunt.log.writeln("Setting directory to: " + directory);
-        grunt.config.set('output', directory);
-    });
-
-};
