9c8db0017046dc64c4e18e535db2159ac1596465
Travis McHattie <travis.mchattie@thinksolid.com>
2013-06-19 11:28:57 -0700

    Setup connect code to give examples of static serving mixed with core code.

--- app/grasshopper.js
+++ app/grasshopper.js
@@ -1,8 +1,38 @@
-var http = require('http');
+var connect = require('connect')
+    , http = require('http');
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World!\n');
-}).listen(8124);
+/*
+ * You are adding code but doing the same thing. Why would you use connect?
+ * By adding connect then you get access to all of it's created middleware
+ *      * csrf
+ *      * basicAuth
+ *      * bodyParser
+ *      * json
+ *      * multipart
+ *      * urlencoded
+ *      * cookieParser
+ *      * directory
+ *      * compress
+ *      * errorHandler
+ *      * favicon
+ *      * limit
+ *      * logger
+ *      * methodOverride
+ *      * query
+ *      * responseTime
+ *      * session
+ *      * static
+ *      * staticCache
+ *      * vhost
+ *      * subdomains
+ *      * cookieSession
+ */
+var app = connect()
+    .use(connect.static(__dirname + '/public'))
+    .use(function(req, res){
+        res.end('Hello World!\n');
+    });
+
+http.createServer(app).listen(8124);
 
 console.log('Server running at http://127.0.0.1:8124/');
